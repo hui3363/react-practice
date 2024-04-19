39ecc36f031fabae94fd11d11e341e384fc958d7
seohee <hui3363@gmail.com>
2024-04-19 12:01:35 +0900

    기본 계산기 기능 완성

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -2,22 +2,38 @@ import React from 'react'
 import { MdSend } from 'react-icons/md'
 import "./ExpenseForm.css"
 
-const ExpenseForm = () => {
+const ExpenseForm = ({handleCharge, charge, edit, handleAmount, amount, handleSubmit}) => {
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<div className='form-center'>
 				<div className='form-group'>
 					<label htmlFor='charge'>지출 항목</label>
-					<input type="text" className="form-control" id="charge" name="charge" placeholder='예) 렌트비'></input>
+					<input 
+						type="text" 
+						className="form-control" 
+						id="charge" 
+						name="charge" 
+						value={charge}
+						placeholder='예) 렌트비'
+						onChange={handleCharge}
+					/>
 				</div>
 				<div className='form-group'>
 					<label htmlFor='amount'>비용</label>
-					<input type="number" className="form-control" id="amount" name="amount" placeholder='예) 100'></input>
+					<input 
+						type="number" 
+						className="form-control" 
+						id="amount" 
+						name="amount" 
+						value={amount}
+						placeholder='예) 100'
+						onChange={handleAmount}
+					/>
 				</div>
 			</div>
 
 			<button type='submit' className='btn'>
-				제출
+				{edit? '수정' : '제출'}
 				<MdSend className='btn-icons' />
 			</button>
 		</form>
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,24 +3,28 @@ import ExpenseItem from './ExpenseItem'
 import { MdDelete } from 'react-icons/md'
 import "./ExpenseList.css"
 
-const ExpenseList = ({ handleDelete, initialExpenses }) => {
+const ExpenseList = ({ handleDelete, expenses, handleEdit, clearItems }) => {
 	return (
 		<React.Fragment>
 			<ul className='list'>
-				{initialExpenses.map(expense => {
+				{expenses.map(expense => {
 					return (
 						<ExpenseItem
 							expense={expense}
 							key={expense.id}
 							handleDelete={handleDelete}
+							handleEdit={handleEdit}
 						/>
 					)
 				})}
 			</ul>
-			<button className='btn'>
-				목록 지우기
-				<MdDelete />
-			</button>
+			{expenses.length > 0 && (
+				<button className='btn' onClick={clearItems}>
+					목록 지우기
+					<MdDelete />
+				</button>
+			)}
+			
 		</React.Fragment>
 	)
 
