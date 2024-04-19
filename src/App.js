928278abe34cb929b3bcc2939d575e436f81f961
seohee <hui3363@gmail.com>
2024-04-19 09:56:24 +0900

    클래스형 -> 함수형

--- src/App.js
+++ src/App.js
@@ -1,51 +1,47 @@
-import { Component } from "react";
+import { useState } from "react";
 import "./App.css"
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
 
-class App extends Component {
+const App = () => {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			expenses: [
-				{ id: 1, charge: "렌트비", amount: 1600 },
-				{ id: 2, charge: "교통비", amount: 400 },
-				{ id: 3, charge: "식비", amount: 1200 }
-			]
-		}
+	const [expenses, setExpenses] = useState([
+		{ id: 1, charge: "렌트비", amount: 1600 },
+		{ id: 2, charge: "교통비", amount: 400 },
+		{ id: 3, charge: "식비", amount: 1200 }
+	])
+
+
+	const handleDelete = (id) => {
+		const newExpenses = expenses.filter(expense => expense.id !== id);
+		setExpenses(newExpenses);
 	}
 
-	handleDelete = (id) => {
-		const newExpenses = this.state.expenses.filter(expense => expense.id !== id);
-		this.setState({ expenses: newExpenses});
-	}
+	return (
+		<main className="main-container">
+			<h1>예산 계산기</h1>
 
-	render() {
-		return(
-			<main className="main-container">
-				<h1>예산 계산기</h1>
-				
-				<div style={{width: '100%', backgroundColor: 'white', padding: '1rem'}}>
-					{/* Expense Form */}
-					<ExpenseForm />
-				</div>
+			<div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
+				{/* Expense Form */}
+				<ExpenseForm />
+			</div>
 
-				<div style={{width: '100%', backgroundColor: 'white', padding: '1rem'}}>
-					{/* Expense List */}
-					<ExpenseList initialExpenses={this.state.expenses}
-					handleDelete={this.handleDelete} />
-				</div>
-				
-				<div style={{display: 'flex', justifyContent: 'end', marginTop: '1rem'}}>
-					<p style={{fontSize: '2rem'}}>
-						총 지출:
-						<span>원</span>
-						</p>
-				</div>
-			</main>
-		)
-	}
+			<div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
+				{/* Expense List */}
+				<ExpenseList 
+				  initialExpenses={expenses}
+					handleDelete={handleDelete} 
+				/>
+			</div>
+
+			<div style={{ display: 'flex', justifyContent: 'end', marginTop: '1rem' }}>
+				<p style={{ fontSize: '2rem' }}>
+					총 지출:
+					<span>원</span>
+				</p>
+			</div>
+		</main>
+	)
 }
 
 export default App;
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,30 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ExpenseItem from './ExpenseItem'
 import { MdDelete } from 'react-icons/md'
 import "./ExpenseList.css"
 
-export class ExpenseList extends Component {
-	render() {
-		return (
-			<React.Fragment>
-				<ul className='list'>
-					{this.props.initialExpenses.map(expense => {
-						return (
-							<ExpenseItem 
-								expense={expense} 
-								key={expense.id}
-								handleDelete={this.props.handleDelete}
-							/>
-						)
-					})}
-				</ul>
-				<button className='btn'>
-					목록 지우기
-					<MdDelete />
-				</button>
-			</React.Fragment>
-		)
-	}
+const ExpenseList = ({ handleDelete, initialExpenses }) => {
+	return (
+		<React.Fragment>
+			<ul className='list'>
+				{initialExpenses.map(expense => {
+					return (
+						<ExpenseItem
+							expense={expense}
+							key={expense.id}
+							handleDelete={handleDelete}
+						/>
+					)
+				})}
+			</ul>
+			<button className='btn'>
+				목록 지우기
+				<MdDelete />
+			</button>
+		</React.Fragment>
+	)
+
 }
 
 export default ExpenseList
